d606177329b78248bd5d15c3fedaf1fce8e2fe0f
Dashboard <ayoubahtouch11@gmail.com>
2024-09-26 18:58:31 +0200

    boucles

---
+++ boucles 4.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a , x ;
+    printf("tape la condition:");
+    scanf("%d",&a);
+    for(x=0;x<a;x++){
+        printf("%d \n",2*x+1);
+        
+    }
+   
+
+    return 0;
+}
