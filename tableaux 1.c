131751a4d599cbc804f1b5ac00cdd1925c1ed07b
Dashboard <ayoubahtouch11@gmail.com>
2024-09-27 11:39:08 +0200

    tableaux 1

---
+++ tableaux 1.c
@@ -0,0 +1,17 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+  int nombre[5];
+  int i;
+  i=0;
+  
+  for (i = 0; i < 5; i++) {
+      nombre[i]=0;
+    printf("nombre[%d]= %d \n",i,nombre[i]);
+    
+  }
+  
+
+    return 0;
+}
